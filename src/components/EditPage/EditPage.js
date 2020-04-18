f1f706fd4f555ff2d1e6a989536c656a99ed500a
mustafaibrahim4 <4MustafaIbrahim@gmail.com>
2020-04-18 15:56:36 -0500

    new movie information now being stored

--- src/components/EditPage/EditPage.js
+++ src/components/EditPage/EditPage.js
@@ -5,16 +5,38 @@ import { connect } from 'react-redux';
 
 export class EditPage extends Component {
     state = {
-
+        newMovie: {
+            title: '',
+            description: ''
+        }
     }
 
-
-
+    handleChange = (propertyName, event) => {
+        this.setState({
+            ...this.state.newPlant,
+            [propertyName]: event.target.value
+        })
+    }
+    handleSubmit = () => {
+        console.log('new movie info  is ', this.state);
+        this.props.dispatch({ type: 'PUT_MOVIES', payload: { id: this.props.selectedMovies.id, newMovie: this.state } });
+        this.setState({
+            newMovie: {
+                title: '',
+                description: ''
+            }
+        })
+    }
     render() {
         return (
             <div>
-                <input></input>
+                <form onSubmit={this.handleSubmit}>
+                    <input placeholder="title" type='text' value={this.state.title} onChange={(event) => this.handleChange('title', event)} />
+                    <input placeholder="description" type='text' value={this.state.description} onChange={(event) => this.handleChange('description', event)} />
 
+
+                    <button type='submit' value='update  movie'>UPDATE MOVIE</button>
+                </form>
             </div>
         )
     }
